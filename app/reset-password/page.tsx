becfe64b92065db38881d7c8f666f78a10ade1bc
Aakash Roy <aakashroy@MacBookAir.rocket-fibre.router>
2025-06-18 21:48:43 +0100

    forgot password functionility

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Suspense, useState } from "react";
+import { Suspense, useState, useEffect } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
