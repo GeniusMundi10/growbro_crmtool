bc1bf2eee05e89edfbb141bcfe74e378676b2dd2
Aakash Roy <aakashroy@MacBookAir.rocket-fibre.router>
2025-06-26 18:36:05 +0100

    My Profile heading

--- app/(main)/account/page.tsx
+++ app/(main)/account/page.tsx
@@ -5,12 +5,7 @@ import AccountSettings from "./account-settings"
 export default function AccountPage() {
   return (
     <div className="min-h-screen bg-white">
-      <Header title="Account Settings" />
-      <div className="mb-4">
-        <Link href="/account/settings" className="text-blue-600 hover:underline">
-          Account Settings
-        </Link>
-      </div>
+      <Header title="My Profile" />
       <div className="container mx-auto px-4 py-6">
         <AccountSettings />
       </div>
